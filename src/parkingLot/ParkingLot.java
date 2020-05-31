584ba346013897b80c7927af289d2c6c55c5a8e5
wasim919 <wasimwik919@gmail.com>
2020-05-31 22:24:52 +0530

    Parking lot class added

--- src/parkingLot/ParkingLot.java
+++ src/parkingLot/ParkingLot.java
@@ -8,6 +8,7 @@ package parkingLot;
 import car.Car;
 import java.util.ArrayList;
 
+// Create parking lot class that has all the attributes and behaviours associated with a Parking Lot
 public class ParkingLot {
     int capacity = 0;
     int slotId = 0;
@@ -18,45 +19,45 @@ public class ParkingLot {
     public ParkingLot() {
     }
 
-    public int createParkingLot(int var1) {
-        this.capacity = var1;
-        this.slots = new int[var1];
-        this.slotsCar = new Car[var1];
+    public int createParkingLot(int capacity) {
+        this.capacity = capacity;
+        this.slots = new int[capacity];
+        this.slotsCar = new Car[capacity];
 
-        for(int var2 = 0; var2 < var1; ++var2) {
-            this.slots[var2] = -1;
+        for(int i = 0; i < capacity; ++i) {
+            this.slots[i] = -1;
         }
 
         return this.capacity;
     }
 
     public int getEmptySlot() {
-        for(int var1 = 0; var1 < this.capacity; ++var1) {
-            if(this.slots[var1] == -1) {
-                return var1;
+        for(int i = 0; i < this.capacity; ++i) {
+            if(this.slots[i] == -1) {
+                return i;
             }
         }
 
         return -1;
     }
 
-    public int park(String var1, String var2) {
+    public int park(String regNumber, String colour) {
         if(this.nOfOccupiedSlots < this.capacity) {
-            int var3 = this.getEmptySlot();
-            this.slots[var3] = var3;
-            this.slotsCar[var3] = new Car(var1, var2);
+            int emptySlotId = this.getEmptySlot(); //emptySlotId
+            this.slots[emptySlotId] = emptySlotId;
+            this.slotsCar[emptySlotId] = new Car(regNumber, colour);
             ++this.slotId;
             ++this.nOfOccupiedSlots;
-            return var3 + 1;
+            return emptySlotId + 1;
         } else {
             return -1;
         }
     }
 
-    public boolean leave(int var1) {
-        if(this.nOfOccupiedSlots > 0 && this.slots[var1] != -1) {
-            this.slots[var1 - 1] = -1;
-            this.slotsCar[var1 - 1] = null;
+    public boolean leave(int slotId) {
+        if(this.nOfOccupiedSlots > 0 && this.slots[slotId] != -1) {
+            this.slots[slotId - 1] = -1;
+            this.slotsCar[slotId - 1] = null;
             --this.nOfOccupiedSlots;
             return true;
         } else {
@@ -67,46 +68,45 @@ public class ParkingLot {
     public void status() {
         System.out.println("Slot No.\tRegistration No.\tColor");
 
-        for(int var1 = 0; var1 < this.slots.length; ++var1) {
-            if(this.slots[var1] != -1) {
-                System.out.println(String.format("%d \t\t %s \t\t %s", new Object[]{Integer.valueOf(var1 + 1), this.slotsCar[var1].regNumber, this.slotsCar[var1].color}));
+        for(int i = 0; i < this.slots.length; ++i) {
+            if(this.slots[i] != -1) {
+                System.out.println(String.format("%d \t\t %s \t\t %s", this.slots[i], this.slotsCar[i].regNumber, this.slotsCar[i].color));
+            }
+        }
+    }
+
+    public ArrayList<String> getRegNumberFromColor(String colour) {
+        ArrayList regNumbers = new ArrayList();
+
+        for(int i = 0; i < this.slots.length; ++i) {
+            if(this.slots[i] != -1 && this.slotsCar[i].color.equals(colour)) {
+                regNumbers.add(this.slotsCar[i].regNumber);
             }
         }
 
+        return regNumbers;
     }
 
-    public ArrayList<String> getRegNumberFromColor(String var1) {
-        ArrayList var2 = new ArrayList();
-
-        for(int var3 = 0; var3 < this.slots.length; ++var3) {
-            if(this.slots[var3] != -1 && this.slotsCar[var3].color.equals(var1)) {
-                var2.add(this.slotsCar[var3].regNumber);
-            }
-        }
-
-        return var2;
-    }
-
-    public int getSlotNumberFromRegNumber(String var1) {
-        for(int var2 = 0; var2 < this.slots.length; ++var2) {
-            if(this.slotsCar[var2].regNumber.equals(var1)) {
-                return var2 + 1;
+    public int getSlotNumberFromRegNumber(String regNumber) {
+        for(int i = 0; i < this.slots.length; ++i) {
+            if(this.slotsCar[i].regNumber.equals(regNumber)) {
+                return i + 1;
             }
         }
 
         return -1;
     }
 
-    public ArrayList<String> getSlotNumberFromColor(String var1) {
-        ArrayList var2 = new ArrayList();
+    public ArrayList<String> getSlotNumberFromColor(String colour) {
+        ArrayList slotNumbers = new ArrayList();
 
-        for(int var3 = 0; var3 < this.slots.length; ++var3) {
-            if(this.slots[var3] != -1 && this.slotsCar[var3].color.equals(var1)) {
-                var2.add(Integer.toString(var3 + 1));
+        for(int i = 0; i < this.slots.length; ++i) {
+            if(this.slots[i] != -1 && this.slotsCar[i].color.equals(colour)) {
+                slotNumbers.add(Integer.toString(i + 1));
             }
         }
 
-        return var2;
+        return slotNumbers;
     }
 
     public void show(String var1) {
@@ -115,7 +115,7 @@ public class ParkingLot {
         if(var1.startsWith("create_parking_lot")) {
             var2 = Integer.parseInt(var1.split(" ")[1]);
             var3 = this.createParkingLot(var2);
-            System.out.println(String.format("Created a parking lot with %d slots", new Object[]{Integer.valueOf(var3)}));
+            System.out.println(String.format("Created a parking lot with %d slots", var3));
         } else {
             String var5;
             if(var1.startsWith("park")) {
@@ -125,13 +125,13 @@ public class ParkingLot {
                 if(var4 == -1) {
                     System.out.println("Sorry, parking lot is full");
                 } else {
-                    System.out.println(String.format("Allocated slot number: %d", new Object[]{Integer.valueOf(var4)}));
+                    System.out.println(String.format("Allocated slot number: %d", var4));
                 }
             } else if(var1.startsWith("leave")) {
                 var2 = Integer.parseInt(var1.split(" ")[1]);
                 boolean var7 = this.leave(var2);
                 if(var7) {
-                    System.out.println(String.format("Slot number %d is free", new Object[]{Integer.valueOf(var2)}));
+                    System.out.println(String.format("Slot number %d is free", var7));
                 }
             } else if(var1.startsWith("status")) {
                 this.status();
